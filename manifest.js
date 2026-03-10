91715bb8c7386fc3c5695ca98c3b996650a472f9
ritishsridhar <ritishsridhar@gmail.com>
2026-03-10 13:40:09 +0530

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,22 @@
+{
+  "name": "Ventrion Diagnostics",
+  "short_name": "Ventrion",
+  "start_url": "/index.html",
+  "display": "standalone",
+  "background_color": "#fdfdfd",
+  "theme_color": "#A51C30",
+  "icons": [
+    {
+      "src": "vdx enhanced.png",
+      "sizes": "192x192",
+      "type": "image/png",
+      "purpose": "any"
+    },
+    {
+      "src": "vdx enhanced.png",
+      "sizes": "512x512",
+      "type": "image/png",
+      "purpose": "maskable"
+    }
+  ]
+}
